29ec8ad5cd3bb09cc9ab5755250b454fb8257717
Rohan McGovern <rmcgover@redhat.com>
2017-04-21 14:09:03 +1000

    Clean up boilerplate from setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='pulpstatus',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      description='Pulp Status',
+      author='Rohan McGovern',
+      author_email='rmcgover@redhat.com',
+      url='https://github.com/rohanpm/pulpstatus',
      )
