43009d661aa2a6cbf89377d09096275f015df4a3
Rohan McGovern <rohan@mcgovern.id.au>
2019-02-01 18:37:09 +1000

    Improve AppBody coverage

--- pulpstatus/js-src/app-body.spec.ts
+++ pulpstatus/js-src/app-body.spec.ts
@@ -7,11 +7,43 @@ describe('AppBody', () => {
         const body = new AppBody({});
         expect(body).to.be.an('object');
         expect(!!body).to.be.true;
-    })
+    });
 
-    it('can render', () => {
+    it('can render - empty', () => {
         const body = new AppBody({});
         const rendered = body.render();
         expect(!!rendered).to.be.true;
-    })
+    });
+
+    it('can render - with data', () => {
+        const body = new AppBody({});
+        Object.assign(body.state, {
+            availableEnvs: ['env1', 'env2', 'env3'],
+            env: 'env2',
+        });
+        const rendered = body.render();
+        expect(!!rendered).to.be.true;
+    });
+
+    it('can render - with error', () => {
+        const body = new AppBody({});
+        Object.assign(body.state, {
+            availableEnvs: ['env1', 'env2', 'env3'],
+            env: 'env2',
+            fetchError: 'something went wrong',
+        });
+        const rendered = body.render();
+        expect(!!rendered).to.be.true;
+    });
+
+    it('can render - with charts', () => {
+        const body = new AppBody({});
+        Object.assign(body.state, {
+            availableEnvs: ['env1', 'env2', 'env3'],
+            env: 'env2',
+            charts: 'full',
+        });
+        const rendered = body.render();
+        expect(!!rendered).to.be.true;
+    });
 });
